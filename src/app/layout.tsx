b76804be4ed52e67a3d6166ce75c3a0636eec488
teopopescu <teodor.popescu94@yahoo.com>
2024-06-10 10:52:28 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/NavBar'
 import './globals.css'
+import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={cn('min-h-screen font-sans antialiased grainy',inter.className)}>
+        <Navbar/>
+        {children}</body>
     </html>
   )
 }
