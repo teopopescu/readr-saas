7c5e7b0c34ed49af8ba668ff06727589117e19f6
teopopescu <teodor.popescu94@yahoo.com>
2024-06-22 09:18:58 +0100

    Add authentication, tRPC setup start and primsa config

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-async-client-component */
+"use client"
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
+
+const Page = async ()=> {
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const origin = searchParams.get('origin')
+
+  const apiResponse = await fetch('/api/whatever')
+  const data = await apiResponse.json()
+
+  const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+    onSuccess: (success) => {
+      if(success) {
+        // the user is synced to db
+        router.push(origin ? `/${origin}` : `/dashboard` )
+      }
+
+    }
+  })
+
+  // trpc.authCallback.useQuery(undefined, {
+  //   onSuccess: ({ success }) => {
+  //     if (success) {
+  //       // user is synced to db
+  //       router.push(origin ? `/${origin}` : '/dashboard')
+  //     }
+  //   },
+  //   onError: (err) => {
+  //     if (err.data?.code === 'UNAUTHORIZED') {
+  //       router.push('/sign-in')
+  //     }
+  //   },
+  //   retry: true,
+  //   retryDelay: 500,
+  // })
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>
+          Setting up your account...
+        </h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Page
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,12 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
 
 const Navbar = () => {
     return (
@@ -14,6 +21,36 @@ const Navbar = () => {
 
             {/**todo: add mobile navbar */}
             <div className='hidden items-center space-x-4 sm:flex'>
+
+                
+                </div>
+              {/*todo: add mobile navbar*/}
+              
+              <div className='hidden items-center space-x-4 sm:flex'>
+                <> 
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}>
+                  Get started{' '}
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+                </>
                 </div>
 
             </div>
